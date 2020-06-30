c37591bdeb36eda31895fd413b83185bdeff3b2b
jorgelfer <jorgeferram17@gmail.com>
2020-06-30 14:32:50 -0500

    comented GPU allocation lines

--- train.py
+++ train.py
@@ -45,9 +45,9 @@ flags.DEFINE_integer('weights_num_classes', None, 'specify num class for `weight
 
 
 def main(_argv):
-    physical_devices = tf.config.experimental.list_physical_devices('GPU')
-    for physical_device in physical_devices:
-        tf.config.experimental.set_memory_growth(physical_device, True)
+%    physical_devices = tf.config.experimental.list_physical_devices('GPU')
+%    for physical_device in physical_devices:
+%        tf.config.experimental.set_memory_growth(physical_device, True)
 
     if FLAGS.tiny:
         model = YoloV3Tiny(FLAGS.size, training=True,
