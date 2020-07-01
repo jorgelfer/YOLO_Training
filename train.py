83c8737d19b18c3e911b958312a69aeee387bdc5
jorgelfer <jorgeferram17@gmail.com>
2020-07-01 15:53:40 -0500

    New ModelCheckpoint callback

--- train.py
+++ train.py
@@ -175,10 +175,9 @@ def main(_argv):
             ReduceLROnPlateau(verbose=1),
             EarlyStopping(patience=3, verbose=1),
             ModelCheckpoint('checkpoints/yolov3_train_{epoch}.tf',
+                            verbose=1,
                             save_weights_only=True,
-                            monitor='loss',
-                            mode='min',
-                            save_best_only=True),
+                            period=1000),
             TensorBoard(log_dir='logs')
         ]
 
