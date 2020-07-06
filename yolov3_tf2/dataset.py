635914f803c4ade1547fe5a4ffda8dd80531bd71
jorgelfer <jorgeferram17@gmail.com>
2020-07-06 14:07:00 -0500

    include inteleave in data pipeline:w

--- yolov3_tf2/dataset.py
+++ yolov3_tf2/dataset.py
@@ -91,26 +91,27 @@ IMAGE_FEATURE_MAP = {
     'image/object/bbox/xmax': tf.io.VarLenFeature(tf.float32),
     'image/object/bbox/ymax': tf.io.VarLenFeature(tf.float32),
     'image/object/class/text': tf.io.VarLenFeature(tf.string),
-    # 'image/object/class/label': tf.io.VarLenFeature(tf.int64),
+    'image/object/class/label': tf.io.VarLenFeature(tf.int64),
     # 'image/object/difficult': tf.io.VarLenFeature(tf.int64),
     # 'image/object/truncated': tf.io.VarLenFeature(tf.int64),
     # 'image/object/view': tf.io.VarLenFeature(tf.string),
 }
 
 
-def parse_tfrecord(tfrecord, class_table, size):
+def parse_tfrecord(tfrecord, size=416):
     x = tf.io.parse_single_example(tfrecord, IMAGE_FEATURE_MAP)
     x_train = tf.image.decode_jpeg(x['image/encoded'], channels=3)
     x_train = tf.image.resize(x_train, (size, size))
 
-    class_text = tf.sparse.to_dense(
-        x['image/object/class/text'], default_value='')
-    labels = tf.cast(class_table.lookup(class_text), tf.float32)
+    #class_text = tf.sparse.to_dense(
+    #    x['image/object/class/text'], default_value='')
+    #labels = tf.cast(class_table.lookup(class_text), tf.float32)
+    labels2 = tf.sparse.to_dense(x['image/object/class/label'])
     y_train = tf.stack([tf.sparse.to_dense(x['image/object/bbox/xmin']),
                         tf.sparse.to_dense(x['image/object/bbox/ymin']),
                         tf.sparse.to_dense(x['image/object/bbox/xmax']),
                         tf.sparse.to_dense(x['image/object/bbox/ymax']),
-                        labels], axis=1)
+                        tf.cast(labels2,tf.float32)], axis=1)
 
     paddings = [[0, FLAGS.yolo_max_boxes - tf.shape(y_train)[0]], [0, 0]]
     y_train = tf.pad(y_train, paddings)
@@ -119,13 +120,16 @@ def parse_tfrecord(tfrecord, class_table, size):
 
 
 def load_tfrecord_dataset(file_pattern, class_file, size=416):
-    LINE_NUMBER = -1  # TODO: use tf.lookup.TextFileIndex.LINE_NUMBER
-    class_table = tf.lookup.StaticHashTable(tf.lookup.TextFileInitializer(
-        class_file, tf.string, 0, tf.int64, LINE_NUMBER, delimiter="\n"), -1)
-
+    #LINE_NUMBER = -1  # TODO: use tf.lookup.TextFileIndex.LINE_NUMBER
+    #class_table = tf.lookup.StaticHashTable(tf.lookup.TextFileInitializer(
+    #    class_file, tf.string, 0, tf.int64, LINE_NUMBER, delimiter="\n"), -1)
     files = tf.data.Dataset.list_files(file_pattern)
-    dataset = files.flat_map(tf.data.TFRecordDataset)
-    return dataset.map(lambda x: parse_tfrecord(x, class_table, size))
+    ds_size = sum(1 for _ in files)
+    return files.interleave(lambda x: tf.data.TFRecordDataset(x).map(parse_tfrecord), cycle_length=ds_size, block_length=16, num_parallel_calls=tf.data.experimental.AUTOTUNE,deterministic=False)
+    #dataset = files.flat_map(tf.data.TFRecordDataset)
+    #for data in dataset:
+    #    parse_tfrecord(data, class_table, size)
+    #return dataset.map(lambda x: parse_tfrecord(x, class_table, size))
 
 
 def load_fake_dataset():
